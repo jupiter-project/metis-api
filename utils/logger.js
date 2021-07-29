ca4adb50bdd552347b0e7f950a3421dde990d1ab
Rene Rey <rene@stackitgroup.com>
2021-07-29 16:19:43 -0700

    updating logger config

--- utils/logger.js
+++ utils/logger.js
@@ -1,13 +1,15 @@
 const { S3StreamLogger } = require('s3-streamlogger');
 
-const {
-  createLogger,
-  format,
-  transports,
-} = require('winston');
+const winston = require('winston');
 require('winston-mongodb');
 const path = require('path');
 const { hasJsonStructure } = require('../utils/utils');
+const { isJson, isArray } = require('../utils/utils');
+
+const tsFormat = () =>{
+  let today = new Date()
+  return today.toISOString().split('T')[0]
+};
 
 const getS3StreamTransport = () => {
   if (
@@ -36,7 +38,7 @@ const getS3StreamTransport = () => {
     });
 
     // AWS transport files
-    return new transports.Stream({
+    return new winston.transports.Stream({
       stream: s3Stream,
     });
   }
@@ -45,33 +47,42 @@ const getS3StreamTransport = () => {
 
 const getMongoDBTransport = () => {
   if (process.env.URL_DB) {
-    return new transports.MongoDB({
+    return new winston.transports.MongoDB({
       level: 'error',
       db: process.env.URL_DB,
       options: {
         useUnifiedTopology: true,
       },
       collection: 'metis-logs',
-      format: format.combine(format.timestamp(), format.json()),
+      format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
     });
   }
   return null;
 };
-
-const getMessageFormat = message => (hasJsonStructure(message)
-  ? JSON.parse(message)
-  : message);
 
 const getLabel = (callingModule) => {
   const parts = callingModule.filename.split(path.sep);
   return path.join(parts[parts.length - 2], parts.pop());
 };
 
-
-// Console logs transport
-const consoleTransport = new transports.Console({
-  level: 'info',
-});
+const customLevels = {
+  levels: {
+    error: 0,
+    warn: 1,
+    info: 2,
+    verbose: 3,
+    debug: 4,
+    sensitiveInfo: 5
+  },
+  colors: {
+    error: 'red',
+    warn: 'yellow',
+    info: 'white',
+    verbose: 'green',
+    debug: 'red',
+    sensitiveInfo: 'blue'
+  }
+};
 
 // Mongo DB transport
 const mongoDbTransport = getMongoDBTransport();
@@ -79,9 +90,24 @@ const mongoDbTransport = getMongoDBTransport();
 const s3Transport = getS3StreamTransport();
 
 // Transport list Array
-const transportList = [
-  consoleTransport,
-];
+const transportList = [];
+
+
+if (process.env.CONSOLE_DEBUG_LEVEL) {
+  const consoleDebugLevel = process.env.CONSOLE_DEBUG_LEVEL;
+  transportList.push(
+      new winston.transports.Console({
+        level: consoleDebugLevel
+      })
+  )
+} else {
+  transportList.push(
+      new winston.transports.Console({
+        level: 'debug',
+        timestamp: tsFormat
+      })
+  )
+}
 
 if (s3Transport) {
   transportList.push(s3Transport);
@@ -92,11 +118,16 @@ if (mongoDbTransport && process.env.NODE_ENV === 'production') {
 }
 
 module.exports = function (callingModule) {
-  return createLogger({
-    format: format.combine(
-      format.simple(),
-      format.timestamp(),
-      format.printf(info => `[${info.timestamp}] [${getLabel(callingModule)}] ${info.level} ${getMessageFormat(info.message)}`),
+  return winston.createLogger({
+    levels: customLevels.levels,
+    format: winston.format.combine(
+        winston.format.timestamp({format: 'MM-DD HH:mm:ss'}),
+        winston.format.label({label:'*'}),
+        winston.format.align(),
+        winston.format.simple(),
+        winston.format.printf(({ level, message, label, timestamp }) => {
+          return `${label}${timestamp}|${level}|${getLabel(callingModule)}|${message}`
+        }),
     ),
     transports: transportList,
   });
