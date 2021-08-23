40c8e6d03fccbdbb7635d71c9a81e909cbbfc809
joramirezStackit <jonathan@stackitgroup.com>
2021-08-23 17:09:17 -0500

    Upload channel profile picture

--- controllers/jupiterFSController.js
+++ controllers/jupiterFSController.js
@@ -2,4 +2,6 @@ const JupiterFSService = require('../services/JupiterFSService');
 
 module.exports = (app) => {
   app.post('/v1/api/file', JupiterFSService.fileUpload);
+  app.post('/v1/api/channel/profile', JupiterFSService.fileUpload);
+  app.get('/v1/api/channel/profile', JupiterFSService.channelProfileUpload);
 };
