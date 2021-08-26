36bc00b7b0badc422e40edd2949c82d6bb15e7f8
joramirezStackit <jonathan@stackitgroup.com>
2021-08-26 07:59:52 -0500

    Create endpoint for uploading channel pic

--- controllers/jupiterFSController.js
+++ controllers/jupiterFSController.js
@@ -4,4 +4,5 @@ module.exports = (app) => {
   app.post('/v1/api/file', JupiterFSService.fileUpload);
   app.post('/v1/api/channel/profile', JupiterFSService.channelProfileUpload);
   app.get('/v1/api/channel/profile', JupiterFSService.channelProfileDisplay);
+  app.delete('/v1/api/channel/profile', JupiterFSService.channelProfileDelete);
 };
