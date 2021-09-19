2b8fdbc53e54bb1eb4e4d19878c9824fb5f2c7a0
kubeexample <miguel.villegas@stackitgroup.com>
2021-09-19 14:44:57 -0500

    push notification in android fixed

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const { tokenVerify } = require('./middlewares/authentication');
 const firebaseAdmin = require("firebase-admin");
 
+// Firebase Service initializer
+const firebaseServiceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') ,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+};
+
+module.exports.firebaseAdmin =  firebaseAdmin.initializeApp({
+   credential: firebaseAdmin.credential.cert(firebaseServiceAccount)
+});
 const url = require('url');
 const kue = require('kue');
 const fs = require('fs');
@@ -24,8 +41,6 @@ const pingTimeout = 9000000;
 const pingInterval = 30000;
 
 // Loads job queue modules and variables
-
-
 //@TODO redis needs a password!!!!
 const jobs = kue.createQueue({
   redis: {
@@ -162,24 +177,6 @@ require('./config/api.js')(app);
 const logger = require('./utils/logger')(module);
 
 const mongoDBOptions = { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true };
-
-const firebaseServiceAccount = {
-  type: process.env.FIREBASE_TYPE,
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  // private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') ,
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
-};
-
-// module.exports.firebaseAdmin =  firebaseAdmin.initializeApp({
-//   credential: firebaseAdmin.credential.cert(firebaseServiceAccount)
-// });
-
 
 const {
   serializeUser, deserializeUser, metisSignup, metisLogin,
