c202a0d660bdf99893cc4653761d0a6deaf738b5
Rene <rene@stackitgroup.com>
2022-01-01 06:46:03 -0800

    removed unused Workers directory and files.

--- workers/user.js
+++
@@ -1,8 +0,0 @@
-// // import { gravity } from '../config/gravity';
-// import Worker from './_worker';
-//
-// export default class UserWorker extends Worker {
-//   addToQueue() {
-//     return false;
-//   }
-// }
