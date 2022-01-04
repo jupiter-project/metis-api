250e1c1681324db2851938aa0fd615793d83b935
Rene <rene@stackitgroup.com>
2022-01-04 13:42:14 -0800

    updated axiosConf.js

--- config/axiosConf.js
+++ config/axiosConf.js
@@ -1,9 +1,17 @@
 const axios = require("axios");
 const http = require('http');
 const https = require('https');
+
+// 1 SECONDS = 1000 MS
+// 1 MIN = 1000 * 60 MS
+// 5 MIN = 5 * 1 MIN
+// 5 MIN = 1000 * 60 * 5
+
+const fiveMinutes = 300000;
+
 const config = {
     baseURL: process.env.JUPITERSERVER,
-    timeout: (1000 * 60 * 5), // If the request takes longer than `timeout`, the request will be aborted.
+    timeout: (fiveMinutes), // If the request takes longer than `timeout`, the request will be aborted.
     withCredentials: true,
     //keepAlive pools and reuses TCP connections, so it's faster
     httpAgent: new http.Agent({keepAlive: true, maxSockets: 700}),
